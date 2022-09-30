3849e40d6891ae8359c526ac0a3f410c44a04906
mark <1394768061@qq.com>
2022-09-30 16:04:10 +0800

    1

---
+++ 编程基础/数据结构/线性表/code/SingleLinkLis.cpp
@@ -0,0 +1,16 @@
+
+
+/// @brief 定义单链表结构体
+typedef struct LNode
+{
+    int data;//存放数据
+    struct LNode *next;//存放下一个节点
+} LNode, SingleLinkList;
+
+
+/// @brief 头插法建立单链表
+/// @param L 
+/// @return 
+SingleLinkList ListHeadInsert(SingleLinkList &L){
+    LNode *s,intx;
+}
